fcc724d9c5fe6ee64859f24c6da31ceff69f2dea
tison <wander4096@gmail.com>
2024-04-06 21:20:10 +0800

    post: apache-opendal-graduation (#10)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,12 @@
+import _ from "lodash";
 import rss from '@astrojs/rss';
 import {Site} from "@/config.ts";
 import type {APIContext} from "astro";
 import {getCollection} from "astro:content";
 
-export async function GET(_: APIContext) {
+export async function GET(_ctx: APIContext) {
     const posts = await getCollection('posts');
-
-    const sortedPosts = posts
-        .sort((a, b) =>
-            new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+    const sortedPosts = _.orderBy(posts, (post) => post.data.date, "desc")
 
     let baseUrl = Site.url.toString();
     // removing trailing slash if found
